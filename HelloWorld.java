47614a5009a4d6a53b59ffffaa924abedfdbd9d7
chounese <achou17@amherst.edu>
2016-07-01 20:58:17 -0400

    Just Testing!

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,16 @@ public class HelloWorld{
 	        for( int i = 0; i < 420; i++){
                     System.out.println("Mikey is high as fuck");
                 }
-        }
+        	blazeIt(420);
+    }
+
+    public static void blazeIt(int blazed){
+    	if (blazed==0){
+    		System.out.println("S A D B O Y S");
+    	}
+    	else{
+	    	System.out.println("FIRE");
+	    	blazeIt(blazed-1);
+	    }
+    }
 }
