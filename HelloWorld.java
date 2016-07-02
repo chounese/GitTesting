dec76921b9978af84439d0426a3cb727e8a51aa6
chounese <achou17@amherst.edu>
2016-07-01 20:36:29 -0400

    Alex Commit Merge

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello Alex!");
+		System.out.println("Hello World");
+		System.out.println("Hey Thomas :^)");
+		System.out.println("Hey Billy we merged!");
 	}
 }
